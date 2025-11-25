0681fa5606f0f2ccee1f5d56a3b99935a37fafb1
Augusto Villegas <villevip10@gmail.com>
2025-11-25 19:51:58 -0300

    Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api'
+const baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const api = axios.create({ baseURL })
